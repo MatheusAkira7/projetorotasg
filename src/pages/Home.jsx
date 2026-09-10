1d7ce8a76ee37a5c01aa9704b0a70ac4266a767c
Akira <matheus77akiraaso@gmail.com>
2026-09-10 10:48:49 -0300

    chore:Estrutura inicial do projeto rotas

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+  return (
+    <>
+      <h1>Bem Vindo a Página <span></span></h1>
+    </>
+  )
+}
+
+export default Home
---
+++ src/pages/Produto.jsx
@@ -0,0 +1,11 @@
+
+
+const Produto = () => {
+  return (
+    <>
+      <h1>Nossos Produtos</h1>
+    </>
+  )
+}
+
+export default Produto
