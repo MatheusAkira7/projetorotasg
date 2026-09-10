df13416daa3c1c94cc05e5435b83afd45b757ff5
Akira <matheus77akiraaso@gmail.com>
2026-09-10 11:31:09 -0300

    feat:codificando

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import Sobre from './pages/Sobre'
+import Contato from './pages/Contato'
+import Produto from './pages/Produto'
 
 const App = () => {
   return (
-    <div>
+    <Router>
+      <div className="min-h-screen bg-blue-50 text-gray-900">
+         <NavBar/>
+         <main>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/sobre" element={<Sobre/>}/>
+            <Route path="/Contato" element={<Contato/>}/>
+            <Route path="/Produto" element={<Produto/>}/>
+          </Routes>
+         </main>
+
+      </div>
       
-    </div>
+    </Router>
   )
 }
 
